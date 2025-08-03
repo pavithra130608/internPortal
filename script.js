ee9444e45e7afc0350f4a546007141a8281ce9f9
pavithra <pavi90425@gmail.com>
2025-08-03 12:59:12 +0530

    leader change

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ fetch('https://internportal-backend.onrender.com/api/user')
     document.getElementById('referral').textContent = data.referralCode;
     document.getElementById('donations').textContent = data.totalDonations;
   });
+  fetch('https://internportal-backend.onrender.com/api/leaderboard')
+  .then(res => res.json())
+  .then(data => {
+    const leaderboardContainer = document.getElementById('leaderboard');
+    leaderboardContainer.innerHTML = ''; // Clear old entries
+    data.forEach((user, index) => {
+      const li = document.createElement('li');
+      li.textContent = `${index + 1}. ${user.name} - ${user.donations} points`;
+      leaderboardContainer.appendChild(li);
+    });
+  });
+
